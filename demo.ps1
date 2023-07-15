5139b20f02da6f98d1aaf043f47865f7a552dbb3
DCressey <69086607+DCressey@users.noreply.github.com>
2023-07-15 09:52:31 -0400

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -2,41 +2,46 @@
 .SYNOPSIS
     Produces sample results for Expand-csv
 .NOTES
-    Script:  Results            Rev: 1.1
-    By:   DGC                   Date: 7-12-20
+    Script:  Demo               Rev: 1.3
+    By:   DGC                   Date: 7-15-23
 #>
 
-,   ,
-'    Example 1:  Driver'
-'    ------------------'
+'    Example 1:  CSV File'
+'    --------------------'
 '    '
-Type Grants.csv
-'    '
-Import-Csv Grants.csv | Format-Table -auto 
+Type Invoke.csv
 '   '
+'    Viewed as a table'
+'    '
+Import-Csv Invoke.csv | Format-table -auto 
+'    '
 '    Example 1:  Template'
 '    --------------------'
 '    '
-type grants.tmplt
+type invoke.tmplt
 '    '
 '    Example 1:  Results'
 '    -------------------'
-Expand-csv -Driver Grants.csv -Template Grants.tmplt
+Expand-csv -Driver invoke.csv -Template invoke.tmplt
 
-'   '
-'    Example 2:  Driver'
-'    ------------------'
 '    '
-Type Invoke.csv
+'    Example 2:  CSV File'
+'    --------------------'
 '    '
-Import-Csv Invoke.csv | Format-table -auto 
+Type Grants.csv
+'    '
+'    Viewed as a table'
+'    '
+Import-Csv Grants.csv | Format-Table -auto 
 '    '
 '    Example 2:  Template'
 '    --------------------'
 '    '
-type invoke.tmplt
-'   '
+type grants.tmplt
+'    '
 '    Example 2:  Results'
 '    -------------------'
-Expand-csv -Driver invoke.csv -Template invoke.tmplt
+'    '
+Expand-csv -Driver Grants.csv -Template Grants.tmplt
+'   '
 
