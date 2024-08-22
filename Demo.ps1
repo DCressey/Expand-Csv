a736012f88472e1e407f0ea1dc1aaa24cde104de
David Cressey <69086607+DCressey@users.noreply.github.com>
2024-08-22 09:34:57 -0400

    Update Demo.ps1

--- Demo.ps1
+++ Demo.ps1
@@ -6,31 +6,70 @@
     By:   DGC                   Date: 7-15-23
 #>
 
+
+# Example 1:  Set up input files
+@'
+NAME,ID
+John,18
+Dave,19
+Carmen,20
+Eric,21
+Tom,22
+Lisa,23
+Kyle,24
+'@ | Out-File jars.csv
+
+@'
+java -jar --create -user $name -id $id -file D:/HR/$name-$id-form.pdf
+'@ | Out-file jars.tmplt
+
+
 '    Example 1:  CSV File'
 '    --------------------'
 '    '
-Type Invoke.csv
+Type Jars.csv
 '   '
-'    Viewed as a table'
+'    CSV Data viewed as a table'
 '    '
-Import-Csv Invoke.csv | Format-table -auto 
+Import-Csv Jars.csv | Format-table -auto 
 '    '
 '    Example 1:  Template'
 '    --------------------'
 '    '
-type invoke.tmplt
+
+type Jars.tmplt
 '    '
 '    Example 1:  Results'
 '    -------------------'
-Expand-csv -Driver invoke.csv -Template invoke.tmplt
+Expand-csv -Driver Jars.csv -Template Jars.tmplt
 
+#    Example 2:  Set up input files
+
+@'
+privs,table,user
+ALL,Employees,DBA
+READ,Employees,Analyst
+"READ, WRITE", Employees, Application
+ALL,Departments,DBA
+READ,Departments,"Analyst, Application"
+'@  | Out-File Grants.csv
+
+@'
+grant $privs 
+   on $table 
+   to $user;
+
+'@ | Out-File Grants.tmplt
+
+'    '
+'    '
 '    '
 '    Example 2:  CSV File'
 '    --------------------'
 '    '
 Type Grants.csv
 '    '
-'    Viewed as a table'
+'    CSV DAta viewed as a table'
 '    '
 Import-Csv Grants.csv | Format-Table -auto 
 '    '
@@ -39,6 +78,7 @@ Import-Csv Grants.csv | Format-Table -auto
 '    '
 type grants.tmplt
 '    '
+
 '    Example 2:  Results'
 '    -------------------'
 '    '
