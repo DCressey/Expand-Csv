76247684e2881671d338bd2d31ead96637e05e84
David Cressey <69086607+DCressey@users.noreply.github.com>
2025-07-18 16:56:39 -0400

    Create Demo-2.ps1
    Generates Example-2,  an example with SQL code.

---
+++ Demo-2.ps1
@@ -0,0 +1,75 @@
+<#  
+.SYNOPSIS
+    Produces a sample result for Expand-csv
+.NOTES
+    Script:  Demo-2             Rev: 1.0
+    By:   DGC                   Date: 7-17-25
+#>
+
+<#
+.NOTES
+    Script: Expand-Csv    Rev:  3.2
+    Author: DGC           Date: 2-21-19
+.SYNOPSIS
+    Generates multiple expansions of a template,
+    driven by data in a CSV file.
+.DESCRIPTION
+    This function is a table driven template tool. 
+
+    It generates output from a template and
+    a driver table.  The template file contains plain
+    text and embedded variables.  The driver table 
+    (in a csv file) has one column for each variable, 
+    and one row for each expansion to be generated.
+#>
+function Expand-csv {
+    [CmdletBinding()]
+    Param (
+       [Parameter(Mandatory=$true)] [string] $driver,
+       [Parameter(Mandatory=$true)] [string] $template
+    )
+    Process {
+       Import-Csv $driver | % {
+           $_.psobject.properties | % {Set-variable -name $_.name -value $_.value}
+           Get-Content $template | % {$ExecutionContext.InvokeCommand.ExpandString($_)} 
+       }
+    }
+}
+
+
+#    Example 2:  Set up input files
+
+@'
+privs,table,user
+ALL,Employees,DBA
+READ,Employees,Analyst
+"READ, WRITE", Employees, Application
+ALL,Departments,DBA
+READ,Departments,"Analyst, Application"
+'@  | Out-File Grants.csv
+
+@'
+grant $privs 
+   on $table 
+   to $user;
+
+'@ | Out-File Grants.tmplt
+
+'    '
+'    Example 2:  CSV File'
+'    --------------------'
+'    '
+Type Grants.csv
+'    '
+'    Example 2:  Template'
+'    --------------------'
+'    '
+type grants.tmplt
+'    '
+
+'    Example 2:  Results'
+'    -------------------'
+'    '
+Expand-csv -Driver Grants.csv -Template Grants.tmplt
+'   '
+
